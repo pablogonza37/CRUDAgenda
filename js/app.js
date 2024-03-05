e1a0d3d9e39b296dec374c1e6643426a9807cb01
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-03-04 23:04:29 -0300

    se agrego la logica para crear un nuevo contacto

--- js/app.js
+++ js/app.js
@@ -1,2 +1,33 @@
+import Contacto from "./classContacto.js";
 
+const formularioContacto = document.querySelector("form");
+const nombre = document.querySelector("#nombre"),
+  apellido = document.querySelector("#apellido"),
+  email = document.querySelector("#email"),
+  telefono = document.querySelector("#telefono");
+const agenda = [];
 
+const crearContacto = (e) => {
+  e.preventDefault();
+  console.log("desde la funcion que crea los contactos");
+  const nuevoContacto = new Contacto(
+    nombre.value,
+    apellido.value,
+    email.value,
+    telefono.value
+  );
+  agenda.push(nuevoContacto);
+  console.log(agenda);
+  limpiarFormularioContacto();
+  guardarEnLocalstorage();
+};
+
+const limpiarFormularioContacto = () => {
+  formularioContacto.reset();
+};
+
+const guardarEnLocalstorage = () => {
+  localStorage.setItem("agendaKey", JSON.stringify(agenda));
+};
+
+formularioContacto.addEventListener("submit", crearContacto);
