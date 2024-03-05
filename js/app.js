2e1cb7df2aba8cc88892e2f72fa3aaa66acd23ab
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-03-04 23:18:05 -0300

    se creo la logica para leer los contactos en la tabla

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ const nombre = document.querySelector("#nombre"),
   apellido = document.querySelector("#apellido"),
   email = document.querySelector("#email"),
   telefono = document.querySelector("#telefono");
-const agenda = [];
+  const agenda = JSON.parse(localStorage.getItem('agendaKey')) || [];
 
 const crearContacto = (e) => {
   e.preventDefault();
@@ -19,6 +19,8 @@ const crearContacto = (e) => {
   agenda.push(nuevoContacto);
   console.log(agenda);
   limpiarFormularioContacto();
+  
+  dibujarFila(nuevoContacto, agenda.length);
   guardarEnLocalstorage();
 };
 
@@ -30,4 +32,28 @@ const guardarEnLocalstorage = () => {
   localStorage.setItem("agendaKey", JSON.stringify(agenda));
 };
 
+const dibujarFila = (contacto, numeroFila)=>{
+    const tablaContactos = document.getElementById('tablaContacto');
+    tablaContactos.innerHTML += `<tr>
+    <th scope="row">${numeroFila}</th>
+    <td>${contacto.nombre}</td>
+    <td>${contacto.apellido}</td>
+    <td>${contacto.email}</td>
+    <td>${contacto.telefono}</td>
+    <td>
+      <a class="btn btn-primary" href="./pages/detalleContacto.html">Ver mas</a>
+      <button class="btn btn-warning">Editar</button>
+      <button class="btn btn-danger">Borrar</button>
+    </td>
+  </tr>`
+  }
+
+  const cargaInicial = () =>{
+    if(agenda.length > 0){
+      agenda.map((itemContacto, posicionContacto)=> dibujarFila(itemContacto, posicionContacto + 1))
+    }
+  }
+
 formularioContacto.addEventListener("submit", crearContacto);
+
+cargaInicial();
